cc15d8ef25c96beddd902e5e1b94dd5b5305143d
asaikali <asaikali@gmail.com>
2022-02-11 03:06:23 -0500

    more scripts

---
+++ bin/install-cluster-essentials.sh
@@ -0,0 +1,6 @@
+export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:82dfaf70656b54dcba0d4def85ccae1578ff27054e7533d08320244af7fb0343
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=$TANZU_NET_USERNAME
+export INSTALL_REGISTRY_PASSWORD=$TANZU_NET_PASSWORD
+cd $HOME/tanzu-cluster-essentials
+./install.sh
