20258fdd6c10d3a96badc05bb4f14a85ac49b684
asaikali <asaikali@gmail.com>
2022-02-14 17:14:35 -0500

    Polish

--- bin/remove-cluster-essentials.sh
+++ bin/remove-cluster-essentials.sh
@@ -2,4 +2,8 @@
 
 kapp delete -a secretgen-controller -n tanzu-cluster-essentials
 kapp delete -a kapp-controller -n tanzu-cluster-essentials
+kubectl delete APIService v1alpha1.data.packaging.carvel.dev
 kubectl delete ns tanzu-cluster-essentials
+#kubectl delete ns kapp-controller
+#kubectl delete ns tanzu-package-repo-global
+#kubectl delete ns tanzu-cluster-essentials 
