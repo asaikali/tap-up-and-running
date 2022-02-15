4b03a9e9d90605b95d1ff406bc667405bd154011
asaikali <asaikali@gmail.com>
2022-02-14 21:43:38 -0500

    polish

--- operator-commands/03-add-tanzunet-repo.sh
+++ operator-commands/03-add-tanzunet-repo.sh
@@ -1,13 +1,14 @@
-export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-export INSTALL_REGISTRY_USERNAME=$TANZU_NET_USERNAME
-export INSTALL_REGISTRY_PASSWORD=$TANZU_NET_PASSWORD
-export TAP_VERSION=1.0.1
-
 #
 # Define a repoistory that can be used to pull images 
 # from tanzu net and install them into the cluster
 # the pulling, and beployment of the packages is 
 # handled by kapp-controller and carvel tools 
+
+export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
+export INSTALL_REGISTRY_USERNAME=$TANZU_NET_USERNAME
+export INSTALL_REGISTRY_PASSWORD=$TANZU_NET_PASSWORD
+export TAP_VERSION=1.0.1
+
 tanzu package repository add tanzu-tap-repository \
   --url registry.tanzu.vmware.com/tanzu-application-platform/tap-packages:${TAP_VERSION} \
   --namespace tap-install
